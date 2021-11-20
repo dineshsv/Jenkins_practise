794036543cffcb04869fe99fe189bf9438f1a996
dineshsama <venkatadinesh858@gmail.com>
2021-11-20 08:00:54 +0530

    first commit

---
+++ src/main/java/Jenkins_prac/App.java
@@ -0,0 +1,13 @@
+package Jenkins_prac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
